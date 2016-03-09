f73dd146eb1b0aeeb1d02b5cda36df78604f1ea7
choijaewoong <jwchoi0129@gmail.com>
2016-03-09 19:51:28 +0900

    Toolbar Setting

--- app/src/main/java/com/example/androidchoi/appsearcher/AppListActivity.java
+++ app/src/main/java/com/example/androidchoi/appsearcher/AppListActivity.java
@@ -1,8 +1,11 @@
 package com.example.androidchoi.appsearcher;
 
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
+import android.view.Menu;
 import android.view.WindowManager;
 
 public class AppListActivity extends AppCompatActivity {
@@ -15,5 +18,18 @@ public class AppListActivity extends AppCompatActivity {
         // App 화면 크기 설정
         WindowManager.LayoutParams params = getWindow().getAttributes();
         params.gravity = Gravity.TOP | Gravity.CENTER_HORIZONTAL;
+
+        // Setting Toolbar
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_search);
+        toolbar.setTitleTextColor(ContextCompat.getColor(getBaseContext(), R.color.colorFont));
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.app_search, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }

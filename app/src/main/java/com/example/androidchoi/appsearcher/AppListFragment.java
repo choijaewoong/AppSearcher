edafba160c94a0a2a0f77bcfa7227710e11588bd
choijaewoong <jwchoi0129@gmail.com>
2016-03-11 02:28:54 +0900

    AppList 출력  * ListView로 1차 구현(Recycler View, Card View로 변경 예정)

--- app/src/main/java/com/example/androidchoi/appsearcher/AppListFragment.java
+++ app/src/main/java/com/example/androidchoi/appsearcher/AppListFragment.java
@@ -1,11 +1,22 @@
 package com.example.androidchoi.appsearcher;
 
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 
 /**
@@ -20,7 +31,42 @@ public class AppListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_app_list, container, false);
+        View view =  inflater.inflate(R.layout.fragment_app_list, container, false);
+
+        //암시적 인텐트
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        // appList를 가져오기 위한 PackageManager 객체 생성
+        final PackageManager pm = getActivity().getPackageManager();
+        List<ResolveInfo> appList = pm.queryIntentActivities(intent, 0);
+
+        Log.i("AppList", appList.size() + "");
+
+        // 알파벳 순 정렬
+        Collections.sort(appList, new Comparator<ResolveInfo>() {
+            @Override
+            public int compare(ResolveInfo a, ResolveInfo b) {
+                return String.CASE_INSENSITIVE_ORDER.compare(
+                        a.loadLabel(pm).toString(),
+                        b.loadLabel(pm).toString());
+            }
+        });
+
+        //  appList 출력하는 리스트뷰 구현
+        ListView listView = (ListView)view.findViewById(R.id.listView);
+        listView.setAdapter(new ArrayAdapter<ResolveInfo>(getActivity(), android.R.layout.simple_list_item_1,
+                appList){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView tv = (TextView)view;
+                ResolveInfo ri = getItem(position);
+                tv.setText(ri.loadLabel(pm));
+                return view;
+            }
+        });
+        return view;
     }
 
 

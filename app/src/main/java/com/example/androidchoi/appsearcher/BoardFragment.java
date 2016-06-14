cae44c3ba5c83e066d7bee38bc08ac253077241f
choijaewoong <jwchoi0129@gmail.com>
2016-06-14 13:24:53 +0900

    (BoardFragment) 게시글 작성을 위한 플로팅 버튼 생성

--- app/src/main/java/com/example/androidchoi/appsearcher/BoardFragment.java
+++ app/src/main/java/com/example/androidchoi/appsearcher/BoardFragment.java
@@ -9,12 +9,15 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.clans.fab.FloatingActionButton;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class BoardFragment extends Fragment {
 
+    FloatingActionButton mFloatingActionButton;
 
     public BoardFragment() {
         // Required empty public constructor
@@ -27,6 +30,15 @@ public class BoardFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_board, container, false);
         setHasOptionsMenu(true);
+
+        // 게시글 작성 플로팅 버튼
+        mFloatingActionButton = (FloatingActionButton)view.findViewById(R.id.fab_write_post);
+        mFloatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 작성화면으로 이동
+            }
+        });
         return view;
     }
 

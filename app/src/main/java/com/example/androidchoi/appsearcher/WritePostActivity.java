d28b8fa0729c6b7c989d346e2ecadfefee28f113
choijaewoong <jwchoi0129@gmail.com>
2016-06-14 21:51:56 +0900

    (WritePostActivity) 게시글 저장 메뉴 생성

--- app/src/main/java/com/example/androidchoi/appsearcher/WritePostActivity.java
+++ app/src/main/java/com/example/androidchoi/appsearcher/WritePostActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -31,12 +33,12 @@ public class WritePostActivity extends AppCompatActivity {
         mEditTextTitle = (EditText)findViewById(R.id.editText_post_title);
         mEditTextContent = (EditText)findViewById(R.id.editText_post_content);
 
-//        //제목 EditText에 자동 포커스 및 키보드 세팅
+        //제목 EditText에 자동 포커스 및 키보드 세팅
         mEditTextTitle.requestFocus();
         InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.showSoftInput(mEditTextTitle, InputMethodManager.SHOW_IMPLICIT);
 
-//        // 터치 인터셉터
+        // 터치 인터셉터
         FrameLayout touchInterceptor = (FrameLayout)findViewById(R.id.touchInterceptor);
         touchInterceptor.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -63,5 +65,26 @@ public class WritePostActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    // 게시글 저장 요청 메소드
+    public void writePost(){
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_save, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_save) {
+            // 게시글 저장 요청
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
